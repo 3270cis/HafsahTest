14c5a845361875f0a56c03d772fcdb20a6cce197
hafsahqazi <hafsahqazi@hafsahs-air.gsu.edu>
2018-10-23 17:13:35 -0400

    does this work?

---
+++ GitHubPractice/src/Test/testing.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class testing {
+
+	public static void main(String[] args) {
+		boolean working=true;
+		if(working==true) {
+			System.out.println("this works!");
+		}
+	}
+
+}
