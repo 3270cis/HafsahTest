b25a8c90fc75c37d9b02267c4b6c20b479c50443
k <k@10.1.10.34>
2018-10-28 20:35:04 -0400

    testing 1 2 3

--- GitHubPractice/src/Test/testing.java
+++ GitHubPractice/src/Test/testing.java
@@ -3,9 +3,15 @@ package Test;
 public class testing {
 
 	public static void main(String[] args) {
-		boolean working=true;
+		boolean working = true;
+		boolean itWorks = true;
+		
 		if(working==true) {
 			System.out.println("this works!");
+		}
+		
+		if(itWorks == true) {
+			System.out.println("hello world");
 		}
 	}
 
